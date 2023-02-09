9bdee2e465fb880c4543d6e023929a5cc23f2d2d
user_web <githubaddmin@gmail.com>
2023-02-09 14:03:14 +0530

    timer

--- src/pages/TimerClock.js
+++ src/pages/TimerClock.js
@@ -1,46 +1,56 @@
 import styled from '@emotion/styled';
 import { FormControl, TextField,Button, Box } from '@mui/material'
-import  CountownClock from './Timeing/CountownClock'
 import React, { useEffect, useState } from 'react'
 import './style.css'
 
 
 export default function TimerClock() {
-  const dateLive=new Date().toLocaleDateString("en-GB")
-  const [getDateData,setGetDateData]=useState(dateLive)
-  const timeLive=new Date().toLocaleTimeString()
-  const [liveTime,setLiveTime]=useState(timeLive)
-  const [timeClockForm,setTimeClockForm]=useState({
-    date:getDateData,
-    time:liveTime
-  })
-
-   useEffect(() => {
-   }, [])
-   
-   const UpdateTime=()=>{
-   const  time =new Date().toLocaleTimeString();
-      setLiveTime(time)
-   }
-   const UpdateDate=()=>{
-    const  time =new Date().toLocaleDateString("en-GB");
-    setGetDateData(time)
-    }
-  const hadnleTimeClockOnChange = (e) => {
-    if (e) {
-      const name = e?.target.name;
-      const value = e?.target.value;
-      setTimeClockForm({ ...timeClockForm, [name]: value });
-    }
-  };
-  setInterval(UpdateTime,1000)
-  setInterval(UpdateDate,1000)
-  const hadnleTimeClockSubmit=(e)=>{
-    e.preventDefault();
-    console.log("timeClock",timeClockForm)
-    console.log("liveTime",liveTime)
+  const [hour,setHour]=useState(0)
+  const [min,setMin]=useState(0)
+  const [second,setSecond]=useState(0)
+  const [mSecond,setMSecond]=useState(0)
+  const [stop,setStop]=useState(false)
+  
+  const startTime=()=>{
+    setStop(false)
+  }
+  const stopTime=()=>{
+    setStop(true)
+  }
+  const resetTime=()=>{
+    setHour(0)
+    setMin(0)
+    setSecond(0)
+    setMSecond(0)
   }
 
+  useEffect(() => {
+       var interval=null
+       if(!stop){
+          interval=setInterval(()=>{
+              if(min > 60){
+                  setHour(hour+1)
+                  setMin(0)
+                  clearInterval(interval)
+              }
+              if(second > 60){
+                  setMin(min+1)
+                  setSecond(0)
+                  clearInterval(interval)
+               }
+               if(second <= 60){
+                  setSecond(second+1)
+               }
+          },600)
+       }
+       else{
+          clearInterval(interval )
+       }
+       return()=>{
+          clearInterval(interval)
+       }
+  })
+  
   return (
     <div className='timer-clock'> 
       <h6>Timer Clock</h6>
@@ -86,7 +96,10 @@ export default function TimerClock() {
                   </Button>
                   </Box>
                 </form>
-                <CountownClock/>
+                <h1>{hour}:{min}:{second}</h1>
+        <button onClick={startTime}>Start</button>
+        <button onClick={stopTime}>Stop</button>
+        <button onClick={resetTime}>Reset</button>
                 {/* <BorderLinearProgress variant="determinate" value={50} /> */}
     </div>
   )
