e504530bc1005bb289cb123cb13a239890553f1b
Your Name <you@example.com>
2023-03-29 12:53:25 +0530

    DATAAA

--- src/pages/Profile/Profile.js
+++ src/pages/Profile/Profile.js
@@ -55,6 +55,7 @@ export default function Profile() {
   const [getDataUserData,setGetDataUserData]=useState()
   const [errorForm, setErrorForm] = useState({});
   const [isSubmit, setIsSubmit] = useState(false);
+  const [showData,setShowData]=useState()
 
   const {profiles}=useSelector(res=>res.data)
 
@@ -210,7 +211,7 @@ const imageObject={"image":getDataUserData?.image}
     // dispatch(profilePutApi())
 }
 
-const getApiFunction=async()=>{
+const getApiFunction=()=>{
   const dataUserGet=JSON.parse(sessionStorage.getItem("userData"))
   const dataGEtGEtGEt=dataUserGet?.map((item)=>{return setGetUserDataDataData(item)})
   const getUserDeflutData=profiles?.filter((item)=>dataUserGet?.find(ele=>ele?.E_Id===item?.E_Id))
@@ -226,7 +227,6 @@ const getApiFunction=async()=>{
     console.log("olda",checkData)
     const trueDataCheckData=checkData?.includes(true)
     console.log("ture",trueDataCheckData)
-    setOldUsersData(trueDataCheckData)
     if(profiles){
       const filterData=profiles?.filter((item)=>{return item?.E_Id===getUserDataDataData?.E_Id})
       setUserProfileData(filterData)
@@ -239,7 +239,13 @@ const getApiFunction=async()=>{
 
 useEffect(() => {
   getApiFunction()
-}, []);
+}, [userProfileData]);
+useEffect(() => {
+  const dataUserGetData=JSON.parse(sessionStorage.getItem("userData"))
+  const filterData=profiles?.filter((item)=>{return item?.E_Id===dataUserGetData?.find(ele=>ele?.E_Id===item?.E_Id)})
+  console.log("filterData",filterData)
+  setShowData(filterData)
+}, [])
 
 const userProfileDataFunction=()=>{
    dispatch(profileGetApi())
@@ -286,7 +292,7 @@ const handleImgChange=(e)=>{
 <div>
   <div className='profile-text'>
   <Card sx={{ maxWidth: '80%' }}>
-    {  oldUserData===false &&((
+    {/* {  oldUserData===false &&((
 <CardContent>
       <div className='profile-lable'>
         <Typography gutterBottom  component="div" sx={{fontWeight:"600",fontSize:"16px",color:"#4f4f4f",width:"150px",textTransform:"capitalize"}}>
@@ -331,9 +337,9 @@ const handleImgChange=(e)=>{
       </CardContent>
       ))
       
-    }
-    {
-      oldUserData===true&&((
+    } */}
+    {/* {
+      oldUserData===true&&(( */}
         <div>
         {
          userProfileData&&userProfileData?.map((item)=>{
@@ -435,8 +441,8 @@ const handleImgChange=(e)=>{
          })
         }
         </div>
-      ))
-    }
+      {/* ))
+    } */}
       
       <CardActions>
         <Button size="small" color="primary" onClick={handleOpen}>
@@ -466,7 +472,7 @@ const handleImgChange=(e)=>{
                     {
                      imageUpload?.length===0?
                      <div>
-                     <CardContent variant="contained" component="label" className='upload-img'>   <img src={uploadImgIcon} />
+                     <CardContent variant="contained" component="label" className='upload-img'>   <img src={`https://hopebackend.hopeinfosys.com/${getDataUserData?.image&&getDataUserData?.image}`} />
                    <input hidden   type="file" accept="image/png , image/jepg,.txt,.doc" id='image' name='image'  onChange={handleImgChange} />
                     </CardContent>
                   </div>:<div>
